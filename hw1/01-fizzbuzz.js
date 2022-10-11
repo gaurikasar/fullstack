2a0ad65eedfa9e4507700342c455c53764ecd649
gauri kasar <gaurikasar333@gmail.com>
2022-10-10 21:33:52 -0700

    Commit with formatting changes

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -1,15 +1,12 @@
 const fizzbuzz = () => {
-  for(let i=1;i<=100;i++){
-    if(i%3 ===0 && i%5===0){
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
       console.log("fizzbuzz");
-    }
-    else if(i%3 ===0){
+    } else if (i % 3 === 0) {
       console.log("fizz");
-    }
-    else if(i%5 ===0){
+    } else if (i % 5 === 0) {
       console.log("buzz");
-    }
-    else{
+    } else {
       console.log(i);
     }
   }
