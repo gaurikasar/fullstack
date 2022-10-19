e34d7fe4fb112bddd209b8abc8b0424acdb679f1
gauri kasar <gaurikasar333@gmail.com>
2022-10-19 14:07:10 -0700

    code changes for homework2

--- hw2/02-reverse.js
+++ hw2/02-reverse.js
@@ -1,3 +1,18 @@
 /** Exercise 02 - Reverse **/
 
-// Add your code here
+const calculateReverse = () => {
+  var input = document.getElementById("input").value;
+  var result = document.getElementById("result");
+  let finalans = "";
+  var reverse = 0;
+  if (input.length > 8 || input.length == 0) {
+    finalans = "Error:Please enter valid number which has length equal to 8 ";
+    result.style.color = "red";
+  } else {
+    finalans = input.toString().split("").reverse().join("");
+    result.style.color = "green";
+  }
+
+  result.innerHTML = finalans;
+};
+
--- hw2/03-form.js
+++ hw2/03-form.js
@@ -1,3 +1,17 @@
-/** Exercise 03 - Form **/
 
-// Add your code here
+var form = document.querySelector("form");
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    console.log("Name:", form.elements[0].value);
+    console.log("Email:", form.elements[1].value);
+    if(form.elements[2].value!=""){
+        console.log("Feedback:",form.elements[2].value);
+    }
+    if(form.elements[3].checked){
+         console.log("Yes,I would like to join newsletter");
+    }
+    else {
+         console.log("No,thankyou");
+    }
+    
+  });
