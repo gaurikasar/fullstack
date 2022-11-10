bdfc3cf37d4a0aceb9ba0009101bdb381070e095
gaurikasar <gaurikasar333@gmail.com>
2022-11-09 16:24:15 -0800

    Revert "Code changes for homework 3"

--- hw4/02-sessions.js
+++ hw4/02-sessions.js
@@ -1,44 +1,17 @@
-const express = require("express");
-const session = require("express-session");
+const express = require('express');
+const session = require('express-session');
 const app = express();
-const port = process.env.PORT || 5002;
+const port = process.env.PORT || 5000;
 
 // Add your code here
 
 // Use the express-session module
-app.use(
-  session({
-    store: new session.MemoryStore(),
-    secret: "a secret to sign the cookie",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 86400000,
-    },
-  })
-);
+app.use(/** ... */);
 
-app.get("/", (req, res) => {
-  res.status(200);
-  res.set({ "Content-Type": "text/plain" });
-  req.session.example = [];
-  req.session.example.push("/");
-  res.send("Currently on home route / \n");
+app.get('/', (req, res) => {
+  // Add your code here
 });
-app.get("/:character", (req, res) => {
-  res.status(200);
-  res.set({ "Content-Type": "text/plain" });
-  let current = "Currently on route: " + req.url + "\n";
-  //res.send(req.session.example);
-  let ans = "Previously Visited";
-  //res.send('Previously visited');
-  req.session.example.forEach((element) => {
-    ans += element + "\n";
-  });
-  req.session.example.push(req.url);
-  res.send(current + ans);
-  console.log(req.session.example);
-});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
